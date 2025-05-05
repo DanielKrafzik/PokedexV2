b01b231be8e3590a7618470c40f6f4cc5215daac
danielkrafzik <daniel.krafzik@hotmail.de>
2025-05-05 14:10:02 +0200

    created base

--- script.js
+++ script.js
@@ -1,16 +1,82 @@
+window.addEventListener('load', () => {
+  fetchPokeDataJson('https://pokeapi.co/api/v2/pokemon/');
+});
+
 let pokeData;
+let pokeCounter = 0;
+let pokeContainerCounter = 0;
+let pokeCounterIndex = 0;
 
-fetch('https://pokeapi.co/api/v2/pokemon/')
-  .then((res) => res.json())
-  .then((data) => {
-    pokeData = data;
-    console.log(pokeData)
-  })
-  .catch((err) => {
-    console.log(err)
-  });
-
-function loadPokemon() {
-    setTimeout(document.getElementById("pokemon").innerHTML = pokeData.result[0].name, 2000)
-    console.log(pokeData)
+async function fetchPokeDataJson(url) {
+  let response = await fetch(url);
+  let responseJson = await response.json();
+  pokeData = responseJson;
+  showPokemon(pokeData);
 }
+
+async function showPokemon(data) { 
+  let pokeInfos = [];
+  for (let i = 0; i < data.results.length; i++) {
+    pokeCounter++;
+    let singlePokeDataUrl = 'https://pokeapi.co/api/v2/pokemon/' + pokeCounter;
+    let pokeName = data.results[i].name;
+    pokeInfos.push(fetchSinglePokeDataJson(singlePokeDataUrl, pokeName));
+  }
+  let allPokeInfos = await Promise.all(pokeInfos);
+  allPokeInfos.forEach(data => {
+    document.getElementById("pokemonContainer").innerHTML += getPokemonTemplate(data.name, data.id, data.img);
+    showPokeTypes(data.types);
+  });  
+  setPokeData(allPokeInfos);
+}
+
+function setPokeData(pokeInfoArray) {
+  for (let i = 0; i < pokeInfoArray.length; i++) {
+        document.querySelectorAll(".pokeCard")[i].dataset.name = pokeInfoArray[i].name;
+        document.querySelectorAll(".pokeCard")[i].dataset.id = pokeInfoArray[i].id;
+        document.querySelectorAll(".pokeCard")[i].dataset.img = pokeInfoArray[i].img;
+        document.querySelectorAll(".pokeCard")[i].dataset.type1 = pokeInfoArray[i].types[0].type.name;
+        if(pokeInfoArray[i].types[1]) {
+          document.querySelectorAll(".pokeCard")[i].dataset.type2 = pokeInfoArray[i].types[1].type.name;
+        }
+  }  
+}
+
+async function fetchSinglePokeDataJson(url, data) {
+  let response = await fetch(url);
+  let responseJson = await response.json();
+  return {
+    name: data,
+    id: responseJson.id,
+    img: responseJson.sprites.front_default,
+    types: responseJson.types,
+  }
+}
+
+function showPokeTypes(pokeTypesData) {
+  for (let i = 0; i < pokeTypesData.length; i++) {
+    document.querySelectorAll(".pokeTypes")[pokeCounterIndex].innerHTML += `<div>${pokeTypesData[i].type.name}</div>`;    
+  }
+  pokeCounterIndex++;
+}
+
+function showNextGroupOfPokemon() {
+  pokeContainerCounter += 20;
+  let nextUrl = `https://pokeapi.co/api/v2/pokemon/?offset=${pokeCounter}&limit=20`;
+  fetchPokeDataJson(nextUrl)
+}
+
+async function fetchSinglePokeInfos(element) {
+  let singlePokeApi = "https://pokeapi.co/api/v2/pokemon/" + element.dataset.id;
+  let response = await fetch(singlePokeApi);
+  let responseJson = await response.json();
+  let pokeHeight = responseJson.height;
+  let pokeWeight = responseJson.weight;
+  let pokeBaseExp = responseJson.base_experience;
+  let abilityArray = responseJson.abilities;
+  showPokeCard(element);
+  showMainInfosTemplate(pokeHeight, pokeWeight, pokeBaseExp);
+  for (let i = 0; i < abilityArray.length; i++) {
+    document.getElementById("abilitySpan").innerHTML += `<p>${abilityArray[i].ability.name}</p>`;    
+  }
+};
---
+++ scripts/template.js
@@ -0,0 +1,109 @@
+function getPokemonTemplate(name, id, img) {
+    return `
+            <div class="pokeCard" onclick="fetchSinglePokeInfos(this)">
+                <div>
+                <p>${id}</p>
+                <p>${name}</p>
+                </div>
+                <img src="${img}">
+                <div class="pokeTypes">
+                </div>
+            </div>
+        `
+}
+
+function showPokeCard(element) {
+    document.getElementById("pokePopUp").innerHTML = `
+                                                      <div>
+                                                        <p>${element.dataset.name}</p>
+                                                        <p>${element.dataset.id}</p>
+                                                        <img src="${element.dataset.img}">
+                                                        <div>
+                                                            <p>${element.dataset.type1}</p>
+                                                            <p id="type2"></p>
+                                                        </div>
+                                                        <div>
+                                                            <p data-id="${element.dataset.id}" onclick="showMainInfos(this)">Main</p>
+                                                            <p data-id="${element.dataset.id}" onclick="showStats(this)">Stats</p>
+                                                        </div>
+                                                        <div id="cardInfoSelection">                                                            
+                                                        </div>
+                                                      </div>                                                      
+                                                    `;
+    showType2(element);
+}
+
+function showType2(element) {
+    if(element.dataset.type2) {
+        document.getElementById("type2").innerText = element.dataset.type2;
+    }
+}
+
+async function showStats(data) {
+  let singlePokeApi = "https://pokeapi.co/api/v2/pokemon/" + data.dataset.id;
+  let response = await fetch(singlePokeApi);
+  let responseJson = await response.json();    
+  document.getElementById("cardInfoSelection").innerHTML = `
+    <div>
+        <span>Hp</span>
+        <span>: ${responseJson.stats[0].base_stat}</span>
+    </div>
+    <div>
+        <span>Attack</span>
+        <span>: ${responseJson.stats[1].base_stat}</span>
+    </div>
+    <div>
+        <span>Defense</span>
+        <span>: ${responseJson.stats[2].base_stat}</span><div>
+    </div>
+    <div>
+        <span>Special-Attack</span>
+        <span>: ${responseJson.stats[3].base_stat}</span>
+    </div>
+    <div>
+        <span>Special-Defense</span>
+        <span>: ${responseJson.stats[4].base_stat}</span>
+    </div>
+    <div>
+        <span>Speed</span>
+        <span>: ${responseJson.stats[5].base_stat}</span>
+    </div>
+`;
+console.log(Object.keys(responseJson));
+
+}
+
+async function showMainInfos(data) {
+    let singlePokeApi = "https://pokeapi.co/api/v2/pokemon/" + data.dataset.id;
+    let response = await fetch(singlePokeApi);
+    let responseJson = await response.json(); 
+    let pokeHeight = responseJson.height;
+    let pokeWeight = responseJson.weight;
+    let pokeBaseExp = responseJson.base_experience;
+    let abilityArray = responseJson.abilities;
+    showMainInfosTemplate(pokeHeight, pokeWeight, pokeBaseExp);
+    for (let i = 0; i < abilityArray.length; i++) {
+        document.getElementById("abilitySpan").innerHTML += `<p>${abilityArray[i].ability.name}</p>`;    
+    }
+}
+
+function showMainInfosTemplate(height, weight, exp) {
+    document.getElementById("cardInfoSelection").innerHTML = `
+        <div>
+            <span>Height</span>
+            <span>: ${height}</span>
+        </div>
+        <div>
+            <span>Weight</span>
+            <span>: ${weight}</span>
+        </div>
+        <div>
+            <span>Base experience</span>
+            <span>: ${exp}</span>
+        </div>
+        <div>
+            <span>Abilitites</span>
+            <span id="abilitySpan">:</span>
+        </div>
+    `
+}
